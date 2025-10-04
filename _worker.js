5f2667169f3460c3a8f19321387440ed65247ebf
maztampandata <maztampandata@gmail.com>
2025-10-04 10:53:16 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -134,6 +134,19 @@ async function getPrxList() {
 }
 
 
+function shuffleArray(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+}
 /* =======================
    PROXY / SUB GENERATOR (ROTATE ONE RANDOM CONFIG from SG+ID with TLS & NTLS)
    ======================= */
